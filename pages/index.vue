13a2a8d718e146e67bba9cb75a19b2142044af6c
Adel Alsaadi <aalsaadi@qewam.com>
2025-01-08 14:44:27 +0300

    update images for home page

--- pages/index.vue
+++ pages/index.vue
@@ -360,13 +360,13 @@ function openModal() {
             </div>
             <div class="order-2 lg:col-span-7">
               <div class="flex bg-gray-50 p-10 rounded-lg">
-                <img class="w-full object-contain h-[400px]" src="/img/screens/Task%20Card%201.svg" alt="">
+                <img class="w-full object-contain h-[400px]" src="/img/screens/Task%20Card%201.png" alt="">
               </div>
             </div>
 
             <div class="order-4 lg:order-3 lg:col-span-7 ">
               <div class="bg-gray-50 p-10 rounded-lg">
-                <img class="w-full object-contain h-[400px]" src="/img/screens/Task%20Card%202.svg" alt="">
+                <img class="w-full object-contain h-[400px]" src="/img/screens/Task%20Card%202.png" alt="">
               </div>
             </div>
 
@@ -393,7 +393,7 @@ function openModal() {
             </div>
             <div class="order-6 lg:col-span-7">
               <div class="bg-gray-50 p-10 rounded-lg">
-                <img class="w-full object-contain h-[400px]" src="/img/screens/Task%20Card%203.svg" alt="">
+                <img class="w-full object-contain h-[400px]" src="/img/screens/Task%20Card%203.png" alt="">
               </div>
             </div>
 
